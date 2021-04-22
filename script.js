f2b3104390b52e1661f81928a56b215b48a46d5f
Johan <johan.rosaperez@gamil.com>
2021-04-21 20:12:25 -0400

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+const actualPage = "https://linktr.ee/Kinethinkrd";
+
+const logoPng =
+  "https://raw.githubusercontent.com/kinethinkrd/educacion_oportuna/master/img/logo.png";
+
+const logoWhite =
+  "https://github.com/kinethinkrd/educacion_oportuna/blob/master/img/logowhite.png";
+
+const logoShortMargin =
+  "https://github.com/kinethinkrd/educacion_oportuna/blob/master/img/kinethink_smallmargin.png";
+
+const linksElements = [
+  { texto: "Amazon list", enlace: "", imagen = ""},
+  { texto: "Programa de estimulación oportuna", enlace: "", imagen = ''},
+  { texto: "Playlist de música variada en versión instrumental", enlace: "", imagen = ''},
+  { texto: "Musica clásica para bebés", enlace: "", imagen = ''},
+];
+
+// Elementos de la lista
+
+ const ids = ['amazon', 'estimulacion', 'instrumental', 'clasica',]
+
+const links = document.getElementsByClassName('link')
+
